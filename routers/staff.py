88b6fbb904035235294fd209612fa12879b2ad44
XYCode Kerman <xycode-xyc@outlook.com>
2024-08-16 00:34:40 +0800

    feat(routers/): 增加员工绑定功能
    * 增加新的API端点 /bind-me 用于将当前用户与员工绑定，如果员工不存在则自动创建。
    * 确保用户已经通过身份验证，并且具有相应的权限。
    * 检查用户是否已经绑定了员工，如果已绑定则抛出 409 状态码。
    * 验证部门 ID 是否有效，如果无效则抛出 404 状态码。
    * 如果部门存在，则创建或更新员工记录，并返回员工信息。

---
+++ routers/staff.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+
+from bson.errors import InvalidId
+from fastapi import APIRouter, Body, Depends, HTTPException
+from fief_client import FiefAccessTokenInfo
+from odmantic import AIOEngine, ObjectId
+
+from models import Section, Staff
+from utils import fief_auth
+from utils.database import get_database
+
+router = APIRouter(tags=["Staff"])
+
+
+@router.post(
+    "/bind-me",
+    name="将当前用户和员工绑定（没有则自动创建）",
+    response_model=Staff,
+    dependencies=[
+        Depends(fief_auth.authenticated(permissions=["oa:staff:bind"])),
+    ],
+)
+async def bind_me(
+    name: Annotated[str, Body()],
+    section_id: Annotated[str, Body()],
+    token_info: FiefAccessTokenInfo = Depends(fief_auth.authenticated()),
+    engine: AIOEngine = Depends(get_database),
+):
+    result = await engine.count(Staff, Staff.user_id == token_info["id"])
+
+    if result > 0:
+        raise HTTPException(status_code=409, detail="该用户已经绑定了员工")
+
+    try:
+        section = await engine.find_one(Section, Section.id == ObjectId(section_id))
+    except InvalidId:
+        raise HTTPException(status_code=404, detail="找不到部门")
+
+    if section is None:
+        raise HTTPException(status_code=404, detail="找不到部门")
+
+    await engine.save(
+        Staff(
+            id=ObjectId(), name=name, tags=[], user_id=token_info["id"], section=section
+        )
+    )
+
+    return engine.find_one(Staff, Staff.user_id == token_info["id"])
